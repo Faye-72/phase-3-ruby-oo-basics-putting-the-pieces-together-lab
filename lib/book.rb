78b94e0407a3198553131dfd30dc93dbda10c759
fmitei <faith.mitei@trustgro.com>
2023-03-31 01:39:53 +0300

    completed the assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,24 @@
 
+class Book
+  
+    attr_accessor :title, :author, :page_count, :genre , :current_page
 
+    def initialize(title)
+        @title=title
+    end
+
+    def properties(author, page_count,genre)
+        @author = author
+        @page_count = page_count
+        @genre = genre
+        @current_page = 1 
+    end
+
+    def turn_page
+        @current_page += 1
+    end
+  
+end
+
+
+# book = Book.new ("The great Gatsby" ,"F. Scott Fitzgerald", 180, "Fiction" )
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,24 @@
 # Make your shoe class here!
+
+class Shoe 
+    attr_reader :brand
+    attr_accessor :shoe , :color , :size ,:material ,:condition
+
+    def initialize(brand)
+        @brand =  brand  
+    end
+    def properties(shoe,color,size, material,condition)
+        @shoe = shoe
+        @color = color
+        @size = size
+        @material = material
+        @condition = condition
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        self.condition = "new"
+    end
+end
+
+cobble = Shoe.new("Nike" )
