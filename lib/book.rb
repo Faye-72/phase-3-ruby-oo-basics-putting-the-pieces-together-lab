195b83170ca07c396a32d4bb6992c94a98a7125a
fmitei <faith.mitei@trustgro.com>
2023-04-01 18:24:07 +0300

    completed test

--- lib/book.rb
+++ lib/book.rb
@@ -1,24 +1,25 @@
 
 class Book
-  
-    attr_accessor :title, :author, :page_count, :genre , :current_page
+    attr_reader :current_page
+    attr_accessor :title, :author, :page_count, :genre
 
     def initialize(title)
         @title=title
+         @current_page = 1 
     end
 
     def properties(author, page_count,genre)
         @author = author
         @page_count = page_count
         @genre = genre
-        @current_page = 1 
+       
     end
 
     def turn_page
+        puts "Flipping the page...wow, you read fast!"
         @current_page += 1
     end
   
 end
 
 
-# book = Book.new ("The great Gatsby" ,"F. Scott Fitzgerald", 180, "Fiction" )
